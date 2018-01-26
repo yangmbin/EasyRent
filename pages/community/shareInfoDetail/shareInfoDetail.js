aa58fee81a3e3a0be1cfc49dfbec7007091401d9
yangmbin <1057207798@qq.com>
2018-01-26 17:29:00 +0800

    接入评论

--- pages/community/shareInfoDetail/shareInfoDetail.js
+++ pages/community/shareInfoDetail/shareInfoDetail.js
@@ -1,7 +1,7 @@
 // pages/community/shareInfoDetail/shareInfoDetail.js
 
 var networkUtil = require('../../../utils/networkUtil.js');
-
+const app = getApp()
 Page({
 
   /**
@@ -13,13 +13,24 @@ Page({
     pay_type: [],
     imageUrl: getApp().globalData.imageUrl,
 
-    hideModal: true
+    hideModal: true,
+    modal_title: '',
+    comment_input: '',
+    // 被回复人的nickname（回复）
+    reply_user_nickname: '',
+    // 被回复的人id（回复）
+    reply_user_id: '',
+    // 被回复的主评论id（回复）
+    share_house_comment_id: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading({
+      title: '加载中',
+    })
     var that = this;
     console.log(options.id)
     var id = options.id
@@ -66,9 +77,11 @@ Page({
         that.setData({
           pay_type: payTypeRes
         })
+
+        wx.hideLoading()
       },
       function (e) {
-
+        wx.hideLoading()
       })
   },
 
@@ -122,15 +135,6 @@ Page({
   },
 
   /**
-   * 提交评论
-   */
-  confirm: function () {
-    this.setData({
-      hideModal: true
-    });
-  },
-
-  /**
    * 取消评论
    */
   cancel: function () {
@@ -144,7 +148,8 @@ Page({
    */
   goComment: function () {
     this.setData({
-      hideModal: false
+      hideModal: false,
+      modal_title: '留言'
     });
   },
 
@@ -179,6 +184,96 @@ Page({
         }
       }
     })
+
+  },
+
+  /**
+   * 点击评论进行回复（可以点击 评论 或 回复）
+   */
+  goReply: function (e) {
+    // 被回复人的nickname
+    var _reply_user_nickname = e.currentTarget.dataset.reply_user_nickname
+    // 被回复的人id
+    var _reply_user_id = e.currentTarget.dataset.reply_user_id
+    // 被回复的主评论id
+    var _share_house_comment_id = e.currentTarget.dataset.share_house_comment_id
+
+    this.setData({
+      hideModal: false,
+      modal_title: '回复 ' + _reply_user_nickname,
+      reply_user_nickname: _reply_user_nickname,
+      reply_user_id: _reply_user_id,
+      share_house_comment_id: _share_house_comment_id
+    });
+  },
+
+  /**
+   * 绑定评论输入框函数
+   */
+  bindCommentInput: function (e) {
+    this.setData({
+      comment_input: e.detail.value
+    })
+  },
+
+  /**
+   * 提交评论或回复
+   */
+  submit_comment: function (e) {
+    if (!app.globalData.openid)
+      return
+
+    wx.showLoading({
+      title: '加载中',
+    })
+
+    var that = this;
+    if (that.data.comment_input == '') {
+      wx.showToast({
+        title: '请输入评论内容',
+      })
+      return
+    }
+
+    // 评论或回复
+    var url = ''
+    var params = {}
+    if (that.data.modal_title == '留言') {
+      url = 'http://127.0.0.1:5000/share_house_comment'
+      params = {
+        'user_id': app.globalData.openid,
+        'share_house_id': that.data.detail.id,
+        'content': that.data.comment_input
+      }
+    } else {
+      url = 'http://127.0.0.1:5000/share_house_reply'
+      params = {
+        'user_id': app.globalData.openid,
+        'reply_user_id': that.data.reply_user_id,
+        'share_house_comment_id': that.data.share_house_comment_id,
+        'content': that.data.comment_input
+      }
+    }
+    networkUtil._post1(url, params,
+      function (e) {
+        wx.hideLoading()
+        wx.showToast({
+          title: e.data.msg,
+        })
+        // 评论成功，刷新当前页面
+        if (e.data.success) {
+          that.setData({
+            hideModal: true
+          });
+          
+          wx.redirectTo({
+            url: 'shareInfoDetail?id=' + that.data.detail.id,
+          })
+        }
+      },
+      function (e) {
+        wx.hideLoading()
+      })
 
   }
 })
